ca9f18ba08b04b05b1d69ef26803091cfdcaa0a9
William Johnson <catfishfishcat10101@gmail.com>
2023-09-09 15:15:05 -0500

    Update SearchForm.js

--- doggos/src/SearchForm.js
+++ doggos/src/SearchForm.js
@@ -1,1 +1,20 @@
+import React from 'React';
 
+class SearchForm extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      inputValue: ''
+    }
+  }
+  render() {
+    return (
+      <>
+      <input placeholder='bread' value={this.state.value} onChange={(e) => this.setState({ inputValue: e.target.value })} />
+      <button onClick={() => this.props.searchDogs(this.state.inputValue)}>Submit</button>
+  </>
+  )
+  }
+}
+
+export default SearchForm;
